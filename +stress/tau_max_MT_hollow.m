c1c61e876931a510f3b7b4f715d72eca8826ba5c
yoosookim04 <yoosoo408@gmail.com>
2026-05-04 13:38:58 +0900

    refactor: +shaft 편의함수 +stress로 이동

--- +stress/tau_max_MT_hollow.m
+++ +stress/tau_max_MT_hollow.m
@@ -3,6 +3,6 @@ function tau_max = tau_max_MT_hollow(M,T,d_2,x)
     % 입력: M 굽힘 모멘트, T 비틀림 모멘트 [Nmm]
     % d_2: 외경 [mm], x: 내경/외경 비율
     % 출력: tau_max = 최대전단응력 [MPa] (등가비틀림모멘트에 대한 비틀림전단응력)
-    T_e = shaft.eq_torque(M,T);
+    T_e = stress.eq_torque(M,T);
     tau_max = stress.torsion_stress_hollow(T_e,d_2,x);
 end
