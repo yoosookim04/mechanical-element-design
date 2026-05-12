b6ed6685ac46c5d74899f937bcd9cc08a934f35b
yoosookim04 <yoosoo408@gmail.com>
2026-05-12 11:06:27 +0900

    feat: 크기계수 K_s 구현, 속도계수 개선

---
+++ +AGMA/K_s.m
@@ -0,0 +1,17 @@
+function [K_s_p, K_s_g] = K_s(b, m_t, N_p, N_g)
+    % Size Factor(크기계수) K_s
+    % 입력: b face width [m], m_t 모듈 (transverse section) [m]
+    %       N_p 피니언 잇수, N_g 기어 잇수 (생략 시 피니언만 계산)
+    % 출력: K_s_p 피니언 크기계수, K_s_g 기어 크기계수 (N_g 입력 시에만)
+
+    if nargin == 3
+        Y_p   = AGMA.form_factor(N_p);
+        K_s_p = 1.192 * (b * m_t * sqrt(Y_p) / 645.16)^0.0535;
+        K_s_g = [];
+        return
+    end
+
+    [Y_p, Y_g] = AGMA.form_factor(N_p, N_g);
+    K_s_p = 1.192 * (b * m_t * sqrt(Y_p) / 645.16)^0.0535;
+    K_s_g = 1.192 * (b * m_t * sqrt(Y_g) / 645.16)^0.0535;
+end
--- +AGMA/K_v.m
+++ +AGMA/K_v.m
@@ -1,9 +1,11 @@
-function K_v = K_v(v,Q_v)
+function K_v = K_v(w, m_t)
     % 속도계수 K_v
-    % 입력: v pich line velocity [m/s] (경험식, fitting), Q_v: 정밀도 등급
+    % 입력: w 각속도 [rad/s], m_t 모듈 (transverse section) [m]
     % 출력: K_v 속도계수
-    B = 0.25*(12-Q_v)^(2/3);
-    A = 50 + 56*(1-B);
+    Q_v = 6;    % 정밀도 등급 6으로 고정
+    v   = w * m_t;  % pitch line velocity [m/s]
+    B   = 0.25*(12-Q_v)^(2/3);
+    A   = 50 + 56*(1-B);
     K_v = ((A+sqrt(200*v))/A)^B;
     % 단위 변환: 1 m/s ≈ 200 ft/min (196.85의 근사, K_v에 미치는 오차 ~0.8%)
 end
